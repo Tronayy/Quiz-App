23a3d2966a2e7c44c11f28ea81f4f85eb53c50fd
Tronayy <usmanfazal001@gmail.com>
2021-08-23 13:52:57 +0500

    Fetch data from backend API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import "./App.css";
-
-const getQuestions = async () => {
-  const response = await fetch(
-    "https://opentdb.com/api.php?amount=10&type=multiple"
-  );
-  const res = response.json();
-  console.log(res);
-};
+import { useCallback, useState } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  const getQuestionsHandler = useCallback(async () => {
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&type=multiple"
+    );
+    const res = await response.json();
+
+    const gotQuestions = res.results.map((quizData) => {
+      return {
+        question: quizData.question,
+        correct_answer: quizData.correct_answer,
+        wrong_answers: quizData.incorrect_answers,
+      };
+    });
+    setQuestions(gotQuestions);
+  }, []);
+
+  if (questions.length > 0) {
+    console.log(questions);
+  }
+
   return (
     <div className="App">
       React Quiz
       <div>
-        <button onClick={getQuestions}>Start Quiz</button>
+        <button onClick={getQuestionsHandler}>Start Quiz</button>
       </div>
     </div>
   );
