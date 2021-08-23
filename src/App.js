df2be74f68e939d2f3aa267e9f160bcf34013394
Tronayy <usmanfazal001@gmail.com>
2021-08-23 17:03:42 +0500

    Correct answer identification

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Card from "./components/Card";
 function App() {
   const [questions, setQuestions] = useState([]);
   const [showBtn, setShowBtn] = useState(true);
+  const [qNo, setQNo] = useState(0);
+  const [chooseAns, setChooseAns] = useState(false);
 
   const getQuestionsHandler = useCallback(async () => {
     const response = await fetch(
@@ -26,13 +28,31 @@ function App() {
   if (questions.length > 0) {
   }
 
+  const nextHandler = () => {
+    var number = qNo + 1;
+    setQNo(number);
+    setChooseAns(false);
+  };
+
   return (
     <div className="App">
       React Quiz
       <div>
         {showBtn && <button onClick={getQuestionsHandler}>Start Quiz</button>}
-        {!showBtn && <Card data={questions} />}
+
+        {!showBtn && (
+          <Card
+            data={questions}
+            number={qNo}
+            choose={chooseAns}
+            fn={setChooseAns}
+          />
+        )}
       </div>
+      <br />
+      {!showBtn && chooseAns && (
+        <button onClick={nextHandler}>Next Question</button>
+      )}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,47 @@
+import { useState, useEffect } from "react";
+
 const Card = (props) => {
+  const [options, setOptions] = useState([]);
+
+  const answer = props.data[props.number].correct_answer;
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  useEffect(() => {
+    if (!props.data[props.number].question) {
+      return;
+    }
+    let answers = [...props.data[props.number].wrong_answers];
+    answers.splice(
+      getRandomInt(props.data[props.number].wrong_answers.length),
+      0,
+      props.data[props.number].correct_answer
+    );
+    setOptions(answers);
+  }, [props.data, props.number]);
+
+  const handleChoose = (event) => {
+    const ans = event.target.innerHTML;
+    if (ans.includes(answer)) {
+      props.fn(true);
+    }
+  };
+
   return (
     <>
-      <p>{props.data[0].question}</p>
+      <p>{props.data[props.number].question}</p>
       <div>
-        <button>{props.data[0].correct_answer}</button>
+        <button onClick={handleChoose}>{options[0]}</button>
       </div>
       <div>
-        <button>{props.data[0].wrong_answers[0]}</button>
+        <button onClick={handleChoose}>{options[1]}</button>
       </div>
       <div>
-        <button>{props.data[0].wrong_answers[1]}</button>
+        <button onClick={handleChoose}>{options[2]}</button>
       </div>
       <div>
-        <button>{props.data[0].wrong_answers[2]}</button>
+        <button onClick={handleChoose}>{options[3]}</button>
       </div>
     </>
   );
