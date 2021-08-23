4b2310d7698fef5c3ae9939add01131189684d2c
Tronayy <usmanfazal001@gmail.com>
2021-08-23 13:35:21 +0500

    Started work on Quiz App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const getQuestions = async () => {
+  const response = await fetch(
+    "https://opentdb.com/api.php?amount=10&type=multiple"
+  );
+  const res = response.json();
+  console.log(res);
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React Quiz
+      <div>
+        <button onClick={getQuestions}>Start Quiz</button>
+      </div>
     </div>
   );
 }
